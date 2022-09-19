7999753d9f6a365641fe0877b0c28b7c60bc862c
pooryaaa <poorya.akbari2@gmail.com>
2022-09-19 19:40:12 +0430

    initialize project

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.rtl.min.css">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="node_modules/vazirmatn/misc/Farsi-Digits/Vazirmatn-FD-font-face.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+<section id="home">
+    <div class="introF">
+        <div>
+            <h1 class="mb-4">
+                SAVIS
+            </h1>
+            <h3 class="mb-3">
+                یک زیبای قدرتمند
+            </h3>
+            <p>
+                ساویس یک قالب وردپرس ساخته و پرداخته یک ایرانیست. قالب وردپرس ساویس را می‌توانید در موضوعات شخصی، رزومه، شرکتی و نمونه کار استفاده کنید. همچنین این قالب با افزونه چندزبانه WPML هماهنگ سازی شده و برای استفاده چندزبانه نیز مناسب است.
+            </p>
+        </div>
+        <div class="socialMedia">
+            <a href="#">
+                <span class="fa fa-instagram"></span>
+            </a>
+        </div>
+    </div>
+</section>
+</body>
+</html>
